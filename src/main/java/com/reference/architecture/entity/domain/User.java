e8135720ad65d4f62ac365c2eaf1eecc4b11d9d8
Sebas <sebas_jc_97@hotmail.com>
2024-09-17 11:56:15 -0500

    database conection

--- src/main/java/com/reference/architecture/entity/domain/User.java
+++ src/main/java/com/reference/architecture/entity/domain/User.java
@@ -1,16 +1,18 @@
 package com.reference.architecture.entity.domain;
 
-public record User(Long id, String name, String lastname, String email) {
+import jakarta.persistence.*;
+import lombok.Data;
 
-    public User {
-        if (name == null || name.isBlank()) {
-            throw new IllegalArgumentException("Name cannot be null or blank");
-        }
-        if (lastname == null || lastname.isBlank()) {
-            throw new IllegalArgumentException("Lastname cannot be null or blank");
-        }
-        if (email == null || email.isBlank()) {
-            throw new IllegalArgumentException("Email cannot be null or blank");
-        }
-    }
+@Entity
+@Data
+@Table(name = "users")
+public class User {
+
+
+    @Id
+    private Long id;
+    private String name;
+    private String lastname;
+    private String email;
+
 }
